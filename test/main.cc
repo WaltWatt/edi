fa8b37188c8c9b9207f2b43c57cc2a561271a1c3
Walt <walt@gbyte.co>
2018-05-06 10:36:34 +1000

    Add diff utility to stdout tests

--- test/main.cc
+++ test/main.cc
@@ -6,16 +6,23 @@
 
 int main()
 {
-	printf("%s", test_name);
-	int dots = 50 - strlen(test_name);
-	while (dots--) {
-		putchar('.');
+	printf("----%s", test_name);
+	int dashes = 54 - strlen(test_name) - 4;
+	while (dashes--) {
+		putchar('-');
 	}
+	puts("");
 
 	int ret = test_main();
 	if (ret) {
+		printf("....%s", test_name);
+		int dots = 50 - strlen(test_name) - 4;
+		while (dots--) {
+			putchar('.');
+		}
 		printf("\033[31mFAIL\033[0m\n");
 	} else {
+		// this never happens btw:
 		printf("\033[32mPASS\033[0m\n");
 	}
 	return ret;
--- test/test_funct.cc
+++ test/test_funct.cc
@@ -5,16 +5,21 @@
 
 #include "test.h"
 
+using namespace std;
+
 const char *test_name = "test/functional";
 
+/**
+ * Get stdandard output of the program (as std::string).
+ */
 std::string exec(const char *cmd)
 {
 	const int buff_size = 128;
 	char buffer[buff_size];
-	std::string result = "";
+	string result = "";
 	FILE *pipe = popen(cmd, "r");
 	if (!pipe) {
-		throw std:: runtime_error("popen() failed!");
+		throw runtime_error("popen() failed!");
 	}
 	try {
 		while (!feof(pipe)) {
@@ -30,11 +35,53 @@ std::string exec(const char *cmd)
 	return result;
 }
 
-int test_main()
+void diff(string name, string expected, string actual)
+{
+	if (expected.compare(actual)) {
+		string s;
+		s.append("Fist failure: \033[36m" + name + "\033[0m" + ":\n");
+		s.append("\033[31m-" + expected + "\033[0m");
+		s.append("\033[32m+" + actual + "\033[0m");
+		cout << s << endl;
+	}
+}
+
+static int test_no_args()
 {
 	const char *cmd = "./edi";
-	const std::string expected = "I'm edi, an awesome new editor.\n";
-	const std::string actual = exec(cmd);
+	const string expected = "I'm edi, an awesome new editor.\n";
+	const string actual = exec(cmd);
 
+        diff(__func__, expected, actual);
 	return expected.compare(actual);
+}
+
+static int test_unknown_args()
+{
+	const char *cmd = "./edi --foo";
+	std::string expected = "edi: Unknown argument \"--foo\"\n";
+	std::string actual = exec(cmd);
+        diff(__func__, expected, actual);
+	return expected.compare(actual);
+
+	/*
+	assert(!expected.compare(actual));
+
+	cmd = "./edi -b";
+	expected.clear();
+	expected.assign("edi: Unknown argument \"-b\"\n");
+	actual = exec(cmd);
+	assert(!expected.compare(actual));
+	*/
+
+	return 0;
+}
+
+
+int test_main()
+{
+
+	return     test_no_args()
+		|| test_unknown_args()
+	;
 }
