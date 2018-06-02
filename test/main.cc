dbcb7532833deec3a2f031ace474e21555a17f3a
Walt <walt@gbyte.co>
2018-06-02 20:54:46 +1000

    Setup googletest

--- test/main.cc
+++ test/main.cc
@@ -1,29 +1,6 @@
-#include <cstring>
+#include <gtest/gtest.h>
 
-#include "test.h"
-
-// ToDo: logging
-
-int main()
-{
-	printf("----%s", test_name);
-	int dashes = 54 - strlen(test_name) - 4;
-	while (dashes--) {
-		putchar('-');
-	}
-	puts("");
-
-	int ret = test_main();
-	if (ret) {
-		printf("....%s", test_name);
-		int dots = 50 - strlen(test_name) - 4;
-		while (dots--) {
-			putchar('.');
-		}
-		printf("\033[31mFAIL\033[0m\n");
-	} else {
-		// this never happens btw:
-		printf("\033[32mPASS\033[0m\n");
-	}
-	return ret;
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
